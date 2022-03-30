89c6e56ec1fc75237f0c72cbd99312b0fe473996
ro_g <97912154+Ro-Galvan@users.noreply.github.com>
2022-03-29 22:20:27 -0400

    pseudo coded out TODO's

--- connection.js
+++ connection.js
@@ -13,25 +13,27 @@
  );
  
 //  // Query database
-
 // put each query into its own function 
- db.query('SELECT * FROM employees', function (err, results) {  //same as doing it in workbench
-   console.log('stuff from db', results); //results can be named whatever we want but usually called results
- });
- db.query('SELECT * FROM employees', function (err, results) {  //same as doing it in workbench
-   console.log('stuff from db', results); //results can be named whatever we want but usually called results
- });
- db.query('SELECT * FROM employees', function (err, results) {  //same as doing it in workbench
-   console.log('stuff from db', results); //results can be named whatever we want but usually called results
- });
- db.query('SELECT * FROM employees', function (err, results) {  //same as doing it in workbench
-   console.log('stuff from db', results); //results can be named whatever we want but usually called results
- });
- db.query('SELECT * FROM employees', function (err, results) {  //same as doing it in workbench
-   console.log('stuff from db', results); //results can be named whatever we want but usually called results
- });
 
- module.exports = db;
+// TODO #1
+// WHEN I choose to view all departments ?
+// THEN I am presented with DEPARTMENT table showing department names and department ids
+//  db.query('SELECT * FROM employees', function (err, results) {  //same as doing it in workbench
+//    console.log('stuff from db', results); //results can be named whatever we want but usually called results
+//  });
+
+
+// TODO #2
+// WHEN I choose to view all roles
+// THEN I am presented with a JOINED TABLE from all 3 tables-- the job title (role), role id (?), the department that role belongs to (department), and the salary (role) for that role
+
+// TODO #3
+// WHEN I choose to view all employees
+// THEN I am presented with a JOINED table from all 3 tables--showing employee data, including employee ids (employee), first names(employee), last names(employee), job titles (role), departments(department), salaries (role), and managers(employee) that the employees report to
+
+// TODO #8:  the video shows the added employee when view all employees is selected--9 employees now
+ 
+module.exports = db;
 
 
 
--- index.js
+++ index.js
@@ -4,7 +4,7 @@
    const db = require('./connection');
 
 
-//    groups some questions
+//    group some questions based on type of questions asked
 // create function and inside function put the array of questions
 
    const initialQuestions = [
@@ -12,7 +12,41 @@
         type: 'list',
         name: 'addmember2',
         message: 'What would you like to do?',
-        choices: ['add an engineer', 'add an intern', 'finished']
+        choices: ['view all departments', 'view all roles', 'view all employees', 
+        'add a department', 'add a role', 'add an employee', 'update an employee role']
+    },
+   ];
+
+   // TODO #4
+// WHEN I choose to add a department
+    // THEN I am prompted to enter TODO A: ASK QUESTION (What is:): the name of the department 
+        // TODO B and that department is added to the database (doesn't show up now in the displayed table)
+
+
+// TODO #5
+// WHEN I choose to add a role
+    // TODO A ASK QUESTIONS: THEN I am prompted to enter the 1.What is the name of the role?, 2.What is their salary? , 3.Which department does the role belong to?
+        // TODO B and that role is added to the database
+
+
+// TODO #6
+// WHEN I choose to add an employee
+    // TODO A ASK QUESTIONS: 1.What is the employee’s first name?, 2.What is the employee’s last name?, 3.What is the employee’s role?, 4.What is the employee’s manager? 
+    // and that employee is added to the database
+
+// TODO #7
+// WHEN I choose to update an employee role
+// THEN I am prompted (ASK ?: Which employee's role do you want to update (show list of all employees)? 2. Which role do you want to assign the selected employee (show list of all roles)? 
+//  and this information is updated in the database ((updated employee's role)
+
+
+   const viewDepartmentQuestions = [
+    {
+        type: 'list',
+        name: 'addmember2',
+        message: 'What would you like to do?',
+        choices: ['view all departments', 'view all roles', 'view all employees', 
+        'add a department', 'add a role', 'add an employee', 'update an employee role']
     },
    ];
 
@@ -20,9 +54,11 @@
 function init() {
     inquirer.prompt(
         [
-            // ...managerQuestions,
+            // ...initialQuestions,
         ]
     )};
+
+
 
 // Function call to initialize app
 init();
